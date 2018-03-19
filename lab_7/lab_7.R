2c81b2dc7683eaf32d4f4621248e0ae2df71c55b
jtrejo13 <jtrejo13@utexas.edu>
2018-03-19 18:08:10 -0500

    Complete lab 7 code

--- lab_7/lab_7.R
+++ lab_7/lab_7.R
@@ -1,57 +1,61 @@
 # Lab7: Quantitative Interaction
 
-# library(SDSRegressionR)
-# 
-# #Load Data
-# vit <- read.csv("data/VitaminD.csv", stringsAsFactors=FALSE)
-# names(vit)
-# 
-# #Run first model for diagnostics FIRST
-# #First run
-# v_mod <- lm(D25 ~ D125 + ageyears + bmi + pth + bmi*ageyears + pth*ageyears, data=vit)
-# summary(v_mod)
-# 
-# #Diagnostics
-# library(car)
-# vif(v_mod)
-# residFitted(v_mod)
-# cooksPlot(v_mod, key.variable = "ID", print.obs=TRUE, sort.obs=TRUE)
-# 
-# #Three bad outliers
-# "%not in%" <- Negate("%in%")
-# g_vit <- vit[vit$ID %not in% c("ID322", "ID156", "ID496"),]
-# 
-# #Re-run
-# v_mod2 <- lm(D25 ~ D125 + ageyears + bmi + pth + bmi*ageyears + pth*ageyears, data=g_vit)
-# summary(v_mod2)
-# 
-# #Find the Simple Slope locations (Pick-a-Point)
-# mean(g_vit$ageyears, na.rm=TRUE)
-# mean(g_vit$ageyears, na.rm=TRUE) - sd(g_vit$ageyears, na.rm=TRUE)
-# mean(g_vit$ageyears, na.rm=TRUE) + sd(g_vit$ageyears, na.rm=TRUE)
-# 
-# #lsmeans for simple slopes
-# library(lsmeans)
-# simple_mns <- lsmeans(v_mod2, "bmi", 
-#                       at=list(bmi=c(0,1), ageyears=c(8.18, 9.87, 11.56)), 
-#                       by="ageyears")
-# simple_mns
-# pairs(simple_mns, reverse=TRUE)
-# 
-# # And Find the Regions of Significance
-# lmDecomp(v_mod2, "bmi", "ageyears", mod.values=c(8.18, 9.87, 11.56), 
-#          print.sslopes=FALSE, print.ros=TRUE)
-# 
-# #Better graphs
-# ROS +
-#   labs(x="Age in Years", title="ROS of Age", subtitle="BMI predicting D25 Serum")
-# 
-# mns <- summary(lsmeans(v_mod2, "bmi", 
-#                        at=list(bmi=seq(10, 30, 1), ageyears=c(8.18, 9.87, 11.56)), 
-#                        by="ageyears"))
-# simpleScatter(g_vit, x=bmi, y=D25, ptalpha = 0,
-#               title="BMI on D25 Moderated by Age") +
-#   geom_line(data=mns, aes(x=bmi, y=lsmean, linetype=factor(ageyears))) +
-#   scale_linetype_manual(name = "Age Values", 
-#                         values = c("dashed", "dotdash", "dotted"), 
-#                         labels = c("One SD Below","Mean of Age", "One SD Above"))
+library(SDSRegressionR)
+
+# Load Data
+conn <- read.csv("data/connected.csv", stringsAsFactors=FALSE)
+names(conn)
+
+# First run
+c_mod <- lm(Problem.Beh ~ age + female + School.Connectedness + Family.Connectedness + Depressive.Sympt + Depressive.Sympt*School.Connectedness, data=conn)
+summary(c_mod)
+
+# Diagnostics
+library(car)
+vif(c_mod)
+residFitted(c_mod)
+cooksPlot(c_mod, key.variable = "STUD_ID", print.obs=TRUE, sort.obs=TRUE)
+threeOuts(c_mod, key.variable = "STUD_ID")
+
+# Outlier Removal
+"%not in%" <- Negate("%in%")
+g_conn <- conn[conn$STUD_ID %not in% c("SID134", "SID192"),]
+summary(g_conn$School.Connectedness)
+mean(g_conn$School.Connectedness)
+sd(g_conn$School.Connectedness)
+
+# Re-run
+c_mod2 <- lm(Problem.Beh ~ age + female + School.Connectedness + Family.Connectedness + Depressive.Sympt + Depressive.Sympt*School.Connectedness, data=g_conn)
+summary(c_mod2)
+
+# Find the Simple Slope locations (Pick-a-Point)
+mean(g_conn$School.Connectedness, na.rm=TRUE)
+mean(g_conn$School.Connectedness, na.rm=TRUE) - sd(g_conn$School.Connectedness, na.rm=TRUE)
+mean(g_conn$School.Connectedness, na.rm=TRUE) + sd(g_conn$School.Connectedness, na.rm=TRUE)
+
+#lsmeans for simple slopes
+library(lsmeans)
+simple_mns <- lsmeans(c_mod2, "Depressive.Sympt",
+                      at=list(Depressive.Sympt=c(0,1), School.Connectedness=c(2.658, 4.358, 6.057)),
+                      by="School.Connectedness")
+simple_mns
+pairs(simple_mns, reverse=TRUE)
+
+# And Find the Regions of Significance
+lmDecomp(c_mod2, "School.Connectedness", "Depressive.Sympt", mod.values=c(2.658, 4.358, 6.057),
+         print.sslopes=FALSE, print.ros=TRUE)
+
+# Better graphs
+ROS +
+  labs(x="School Connectedness", title="ROS of School Connectedness", subtitle="Depressive Symptoms predicting Problem Behavior")
+
+mns <- summary(lsmeans(c_mod2, "Depressive.Sympt",
+                       at=list(Depressive.Sympt=seq(0, 10, 1), School.Connectedness=c(2.658, 4.358, 6.057)),
+                       by="School.Connectedness"))
+simpleScatter(g_conn, x=Depressive.Sympt, y=Problem.Beh, ptalpha = 0,
+              title="Depressive Symptoms on Problem Behavior Moderated by School Connectedness") +
+  geom_line(data=mns, aes(x=Depressive.Sympt, y=lsmean, linetype=factor(School.Connectedness))) +
+  scale_linetype_manual(name = "School Connectedness Values",
+                        values = c("dashed", "dotdash", "dotted"),
+                        labels = c("One SD Below","Mean of School Connectedness", "One SD Above"))
+
